b34c09d5d5e92d952825223792f35dbb3f54fe40
rocallahan <rocallahan@3b7e7dd8-c32e-0410-8ae5-27a6066e960e>
2007-05-28 00:30:13 +0000

    Update register test (still doesn't pass)

--- chronicle/tests/registers.c
+++ chronicle/tests/registers.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 #include <unistd.h>
 
-static long stackPtr;
+static void passStackPtr2(long ptr) {
+  printf("Stackptr2: %p\n", (void*)ptr);
+}
+
+static void printStackPtr2() {
+  int foo;
+  passStackPtr2(&foo);
+}
+
+static void passStackPtr(long ptr) {
+  printf("Stackptr: %p\n", (void*)ptr);
+}
+
+static void printStackPtr() {
+  int foo;
+  passStackPtr(&foo);
+  printStackPtr2();
+}
 
 int main(int argc, char** argv) {
-  int foo;
-  stackPtr = (long)&foo;
-  printf("Stackptr is %p\n", &foo);
+  printStackPtr();
   return 0;
 }
